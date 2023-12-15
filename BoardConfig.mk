d1fa7b85f606714292db4770fedf992d2fb2106d
akifakif32 <mhmtkfdrn@gmail.com>
2023-12-16 00:39:03 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-LG7n
+DEVICE_PATH := device/tecno/LG7n
 
 # Inherit from mt6789-common
 include device/transsion/mt6789-common/BoardConfigCommon.mk
